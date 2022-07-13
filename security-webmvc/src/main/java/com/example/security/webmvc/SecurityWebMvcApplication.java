f6700ba44a389ec1e3f0f995c65a2d1ff19e8240
Moritz Halbritter <mkammerer@vmware.com>
2022-07-13 09:25:40 +0200

    Add 'security-webmvc' smoke test

---
+++ security-webmvc/src/main/java/com/example/security/webmvc/SecurityWebMvcApplication.java
@@ -0,0 +1,13 @@
+package com.example.security.webmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityWebMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityWebMvcApplication.class, args);
+	}
+
+}
---
+++ security-webmvc/src/main/java/com/example/security/webmvc/TestRestController.java
@@ -0,0 +1,29 @@
+package com.example.security.webmvc;
+
+import java.security.Principal;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/rest", produces = MediaType.TEXT_PLAIN_VALUE)
+public class TestRestController {
+
+	@GetMapping("/anonymous")
+	public String anonymous() {
+		return "anonymous";
+	}
+
+	@GetMapping("/authorized")
+	public String authorized(Principal principal) {
+		return "authorized: " + principal.getName();
+	}
+
+	@GetMapping("/admin")
+	public String admin(Principal principal) {
+		return "admin: " + principal.getName();
+	}
+
+}
