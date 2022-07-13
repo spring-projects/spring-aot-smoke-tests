5612db36f5005458ffca4a4061af50b47f7010ca
Moritz Halbritter <mkammerer@vmware.com>
2022-07-13 11:15:18 +0200

    Add 'websocket' smoke test

---
+++ websocket/src/test/java/com/example/websocket/WebSocketApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.websocket;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
