bc56f300243064ad395821a2cb121b40d5a1cfc2
Moritz Halbritter <mkammerer@vmware.com>
2022-07-22 13:54:30 +0200

    Clean up tests

--- logging-log4j2/src/test/java/com/example/log4j2/Log4j2ApplicationTests.java
+++ logging-log4j2/src/test/java/com/example/log4j2/Log4j2ApplicationTests.java
@@ -2,34 +2,13 @@ package com.example.log4j2;
 
 import org.junit.jupiter.api.Test;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
-class Log4j2ApplicationTests implements ApplicationContextAware {
-
-	private ApplicationContext applicationContext;
-
-	@Autowired
-	CLR clr;
-
-	@Override
-	public void setApplicationContext(ApplicationContext applicationContext) {
-		this.applicationContext = applicationContext;
-	}
+class Log4j2ApplicationTests {
 
 	@Test
 	void contextLoads() {
-		assertThat(this.applicationContext).as("ApplicationContextAware").isNotNull();
-	}
-
-	@Test
-	void autowiringWorks() {
-		assertThat(this.clr).as("@Autowired field").isNotNull();
 	}
 
 }
