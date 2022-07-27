a7b27f1ffce2eb312f0b3d6398d2d9f6e6d2817b
Moritz Halbritter <mkammerer@vmware.com>
2022-07-27 11:29:30 +0200

    Update to latest Liberica Native Image Kit

--- ci/images/get-graal-url.sh
+++ ci/images/get-graal-url.sh
@@ -3,7 +3,7 @@ set -e
 
 case "$1" in
 	graal22_1)
-		echo "https://download.bell-sw.com/vm/22.1.0/bellsoft-liberica-vm-openjdk17.0.3+7-22.1.0+1-linux-amd64.tar.gz"
+		echo "https://download.bell-sw.com/vm/22.1.0/bellsoft-liberica-vm-openjdk17.0.3.1+2-22.1.0+2-linux-amd64.tar.gz"
 	;;
   *)
 		echo $"Unknown Graal version"
