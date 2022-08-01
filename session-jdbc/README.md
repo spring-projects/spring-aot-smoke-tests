65ec66eeb5efaee5b825ac6ff3a9df7b579acaa7
Marcus Da Coregio <marcusdacoregio@gmail.com>
2022-08-01 11:16:13 -0300

    Add session-jdbc sample
    Closes gh-27

---
+++ session-jdbc/README.md
@@ -0,0 +1,1 @@
+Tests the session persistence and cookie customization from Spring Session together with Spring Session JDBC and Spring Web.
---
+++ session-jdbc/src/test/java/com/example/session/jdbc/SessionJdbcApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.session.jdbc;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SessionJdbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+	}
+
+}
