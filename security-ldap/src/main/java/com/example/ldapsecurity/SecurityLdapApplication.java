8624f53417e0bf87d93c2a59945f248fec32318a
Marcus Da Coregio <marcusdacoregio@gmail.com>
2022-08-02 13:54:46 -0300

    Add security-ldap sample
    Closes gh-25

---
+++ security-ldap/src/main/java/com/example/ldapsecurity/SecurityLdapApplication.java
@@ -0,0 +1,13 @@
+package com.example.ldapsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityLdapApplication {
+
+	public static void main(String[] args) throws Throwable {
+		SpringApplication.run(SecurityLdapApplication.class, args);
+	}
+
+}
