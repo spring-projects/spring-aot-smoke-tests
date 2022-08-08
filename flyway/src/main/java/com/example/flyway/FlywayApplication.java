89cc9dba110e762542f782e69ed0443444961805
Moritz Halbritter <mkammerer@vmware.com>
2022-08-08 12:29:16 +0200

    Add Hikari hints to flyway sample

--- flyway/src/main/java/com/example/flyway/FlywayApplication.java
+++ flyway/src/main/java/com/example/flyway/FlywayApplication.java
@@ -1,9 +1,12 @@
 package com.example.flyway;
 
+import org.springframework.aot.smoketest.thirdpartyhints.HikariRuntimeHints;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportRuntimeHints;
 
 @SpringBootApplication
+@ImportRuntimeHints(HikariRuntimeHints.class)
 public class FlywayApplication {
 
 	public static void main(String[] args) throws InterruptedException {
