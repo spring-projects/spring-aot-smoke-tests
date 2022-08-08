16aa21e03c432b656f7c78ff70affbcd47148c89
Moritz Halbritter <mkammerer@vmware.com>
2022-08-08 11:58:59 +0200

    Update GraalVM to 22.2
    Closes gh-80

--- ci/images/get-graal-url.sh
+++ ci/images/get-graal-url.sh
@@ -5,6 +5,9 @@ case "$1" in
 	graal22_1)
 		echo "https://download.bell-sw.com/vm/22.1.0/bellsoft-liberica-vm-openjdk17.0.3.1+2-22.1.0+2-linux-amd64.tar.gz"
 	;;
+  graal22_2)
+    echo "https://download.bell-sw.com/vm/22.2.0/bellsoft-liberica-vm-openjdk17.0.4+8-22.2.0+2-linux-amd64.tar.gz"
+  ;;
   *)
 		echo $"Unknown Graal version"
 		exit 1
