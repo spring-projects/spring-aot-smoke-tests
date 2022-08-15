2b450d6e8c1946f6f419b43402cf9913f285d143
Marcus Da Coregio <marcusdacoregio@gmail.com>
2022-08-15 13:59:16 -0300

    Add 'ldap-odm' sample
    See gh-91

---
+++ ldap-odm/src/main/java/example/ldap/odm/PersonController.java
@@ -0,0 +1,62 @@
+package example.ldap.odm;
+
+import java.util.List;
+
+import javax.naming.InvalidNameException;
+import javax.naming.ldap.LdapName;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/person")
+public class PersonController {
+
+	private final PersonDao personDao;
+
+	public PersonController(PersonDao personDao) {
+		this.personDao = personDao;
+	}
+
+	@GetMapping
+	public List<Person> findAll() {
+		return this.personDao.findAll();
+	}
+
+	@GetMapping(params = { "country", "company", "fullName" })
+	public Person showPerson(@RequestParam String country, @RequestParam String company,
+			@RequestParam String fullName) {
+		return this.personDao.findByPrimaryKey(country, company, fullName);
+	}
+
+	@PostMapping
+	public Person addPerson() throws InvalidNameException {
+		Person person = getPerson();
+		this.personDao.create(person);
+		return person;
+	}
+
+	@DeleteMapping(params = { "country", "company", "fullName" })
+	public void removePerson(@RequestParam String country, @RequestParam String company,
+			@RequestParam String fullName) {
+		Person person = this.personDao.findByPrimaryKey(country, company, fullName);
+		this.personDao.delete(person);
+	}
+
+	private Person getPerson() throws InvalidNameException {
+		Person person = new Person();
+		person.setDn(new LdapName("cn=John Doe,ou=company1,c=Sweden,dc=spring,dc=io"));
+		person.setFullName("John Doe");
+		person.setLastName("Doe");
+		person.setCompany("company1");
+		person.setCountry("Sweden");
+		person.setDescription("Test user");
+		person.setPhone("+46 555-123123");
+		return person;
+	}
+
+}
