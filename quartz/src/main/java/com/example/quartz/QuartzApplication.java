299931feb5413a3fedabde30be26cae6526bb1be
Sébastien Deleuze <sdeleuze@vmware.com>
2022-08-18 13:14:47 +0200

    Add required hints for SimpleJob in quartz example
    Will also require oracle/graalvm-reachability-metadata#36 to be green.

--- quartz/src/main/java/com/example/quartz/QuartzApplication.java
+++ quartz/src/main/java/com/example/quartz/QuartzApplication.java
@@ -1,17 +1,31 @@
 package com.example.quartz;
 
+import org.springframework.aot.hint.MemberCategory;
+import org.springframework.aot.hint.RuntimeHints;
+import org.springframework.aot.hint.RuntimeHintsRegistrar;
 import org.springframework.aot.smoketest.thirdpartyhints.HikariRuntimeHints;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportRuntimeHints;
 
 @SpringBootApplication
-@ImportRuntimeHints(HikariRuntimeHints.class)
+@ImportRuntimeHints({ HikariRuntimeHints.class, QuartzApplication.Hints.class })
 public class QuartzApplication {
 
 	public static void main(String[] args) throws InterruptedException {
 		SpringApplication.run(QuartzApplication.class, args);
 		Thread.currentThread().join(); // To be able to measure memory consumption
+	}
+
+	static class Hints implements RuntimeHintsRegistrar {
+
+		@Override
+		public void registerHints(RuntimeHints hints, ClassLoader classLoader) {
+			hints.reflection().registerType(SimpleJob.class,
+					builder -> builder.withMembers(MemberCategory.INVOKE_DECLARED_CONSTRUCTORS,
+							MemberCategory.INVOKE_PUBLIC_METHODS, MemberCategory.DECLARED_FIELDS));
+		}
+
 	}
 
 }
