9c3fa307e9b69c78d31a69a6bafb59bafd3458df
Marcus Da Coregio <marcusdacoregio@gmail.com>
2022-08-25 11:55:11 -0300

    Add security-oauth2-authorization-server sample
    Closes gh-100

---
+++ security-oauth2-authorization-server/src/test/java/com/example/security/oauth2authorizationserver/OAuth2AuthorizationServerApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.security.oauth2authorizationserver;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class OAuth2AuthorizationServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+	}
+
+}
