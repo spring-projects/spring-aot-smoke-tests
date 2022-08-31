1ce85d42611b06e90172c09cf45cca1ca8c47165
Artem Bilan <abilan@vmware.com>
2022-08-31 12:49:22 -0400

    Add a smoke test for Spring Integration
    * The `jni-config.json` is added to support Netty DNS on Windows
    * As well as an `--initialize-at-run-time=sun.net.dns.ResolverConfigurationImpl` in the `build.gradle`
    * The channel based on the Redis with JSON serialization is commented out at the moment since
    there is some deserialization regression in Spring Data Redis.
    Works well with Spring Data `2022.0.0-M4`

---
+++ integration/src/aotTest/java/com/example/integration/IntegrationApplicationTests.java
@@ -0,0 +1,32 @@
+package com.example.integration;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.time.Duration;
+
+import org.awaitility.Awaitility;
+import org.junit.jupiter.api.Test;
+
+import org.springframework.aot.smoketest.support.assertj.AssertableOutput;
+import org.springframework.aot.smoketest.support.junit.AotSmokeTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@AotSmokeTest
+public class IntegrationApplicationTests {
+
+	@Test
+	void shouldOutputIntegrationGraph(WebTestClient client, AssertableOutput output) {
+		client.get().uri("/control-bus/dateSourceEndpoint").exchange().expectStatus().isOk();
+
+		output.assertThat().hasSingleLineContaining("Starting endpoint: dateSourceEndpoint");
+
+		Awaitility.await().atMost(Duration.ofSeconds(30))
+				.untilAsserted(() -> output.assertThat().hasLineContaining("Current seconds:"));
+
+		client.get().uri("/integration-graph").accept(MediaType.APPLICATION_JSON).exchange().expectStatus().isOk()
+				.expectBody(String.class).value(graph -> assertThat(graph).contains("null-channel")
+						.contains("loggingChannel").contains("dateSourceEndpoint"));
+	}
+
+}
---
+++ integration/src/main/java/com/example/integration/IntegrationApplication.java
@@ -0,0 +1,134 @@
+package com.example.integration;
+
+import java.util.Calendar;
+import java.util.Date;
+
+import javax.sql.DataSource;
+
+import org.springframework.aot.smoketest.thirdpartyhints.NettyRuntimeHints;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ImportRuntimeHints;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.http.HttpMethod;
+import org.springframework.integration.annotation.Gateway;
+import org.springframework.integration.annotation.MessagingGateway;
+import org.springframework.integration.annotation.ServiceActivator;
+import org.springframework.integration.channel.interceptor.WireTap;
+import org.springframework.integration.config.EnableIntegrationManagement;
+import org.springframework.integration.config.EnableMessageHistory;
+import org.springframework.integration.config.GlobalChannelInterceptor;
+import org.springframework.integration.config.IntegrationConverter;
+import org.springframework.integration.dsl.IntegrationFlow;
+import org.springframework.integration.dsl.IntegrationFlowDefinition;
+import org.springframework.integration.handler.LoggingHandler;
+import org.springframework.integration.handler.advice.RequestHandlerRetryAdvice;
+import org.springframework.integration.http.config.EnableIntegrationGraphController;
+import org.springframework.integration.jdbc.store.JdbcChannelMessageStore;
+import org.springframework.integration.jdbc.store.channel.H2ChannelMessageStoreQueryProvider;
+import org.springframework.integration.redis.store.RedisChannelMessageStore;
+import org.springframework.integration.support.json.JacksonJsonUtils;
+import org.springframework.integration.webflux.dsl.WebFlux;
+import org.springframework.messaging.MessageHandler;
+
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.simple.SimpleMeterRegistry;
+
+@SpringBootApplication(proxyBeanMethods = false)
+@EnableMessageHistory("dateChannel")
+@EnableIntegrationManagement
+@EnableIntegrationGraphController("/integration-graph")
+@ImportRuntimeHints({ NettyRuntimeHints.class, InterimRuntimeHints.class })
+public class IntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntegrationApplication.class, args);
+	}
+
+	@Bean
+	MeterRegistry simpleMeterRegistry() {
+		return new SimpleMeterRegistry();
+	}
+
+	@Bean
+	JdbcChannelMessageStore jdbcChannelMessageStore(DataSource dataSource) {
+		JdbcChannelMessageStore jdbcChannelMessageStore = new JdbcChannelMessageStore(dataSource);
+		jdbcChannelMessageStore.setChannelMessageStoreQueryProvider(new H2ChannelMessageStoreQueryProvider());
+		return jdbcChannelMessageStore;
+	}
+
+	@Bean
+	RedisChannelMessageStore redisChannelMessageStore(RedisConnectionFactory connectionFactory) {
+		RedisChannelMessageStore redisChannelMessageStore = new RedisChannelMessageStore(connectionFactory);
+		redisChannelMessageStore
+				.setValueSerializer(new GenericJackson2JsonRedisSerializer(JacksonJsonUtils.messagingAwareMapper()));
+		return redisChannelMessageStore;
+	}
+
+	@Bean
+	IntegrationFlow printFormattedSecondsFlow(JdbcChannelMessageStore jdbcChannelMessageStore,
+			RedisChannelMessageStore redisChannelMessageStore) {
+		return IntegrationFlow
+				.fromSupplier(Date::new, e -> e.id("dateSourceEndpoint").poller(p -> p.fixedDelay(1000, 1000)))
+				.channel(c -> c.queue("dateChannel", jdbcChannelMessageStore, "dateChannelGroup"))
+				.gateway(subflow -> subflow.convert(Integer.class, e -> e.advice(new RequestHandlerRetryAdvice())))
+				// .channel(c -> c.queue(redisChannelMessageStore, "secondsChannelGroup"))
+				.handle(m -> System.out.println("Current seconds: " + m.getPayload())).get();
+	}
+
+	@Bean
+	@GlobalChannelInterceptor(patterns = "dateChannel")
+	WireTap loggingWireTap() {
+		return new WireTap("loggingChannel");
+	}
+
+	@Bean
+	@ServiceActivator(inputChannel = "loggingChannel")
+	MessageHandler loggingHandler() {
+		LoggingHandler loggingHandler = new LoggingHandler(LoggingHandler.Level.TRACE);
+		loggingHandler.setLoggerName("tracing.data");
+		return loggingHandler;
+	}
+
+	@Bean
+	@IntegrationConverter
+	Converter<Date, Integer> currentSeconds() {
+		return new Converter<Date, Integer>() { // Not lambda for generic info presence
+
+			@Override
+			public Integer convert(Date date) {
+				Calendar calendar = Calendar.getInstance();
+				calendar.setTime(date);
+				return calendar.get(Calendar.SECOND);
+			}
+
+		};
+	}
+
+	@Bean
+	public IntegrationFlow controlBus() {
+		return IntegrationFlowDefinition::controlBus;
+	}
+
+	@Bean
+	public IntegrationFlow controlBusControllerFlow(ControlBusGateway controlBusGateway) {
+		return IntegrationFlow
+				.from(WebFlux.inboundChannelAdapter("/control-bus/{endpointId}")
+						.payloadExpression("#pathVariables.endpointId")
+						.requestMapping(mapping -> mapping.methods(HttpMethod.GET)))
+				.wireTap(subflow -> subflow.handle(m -> System.out.println("Starting endpoint: " + m.getPayload())))
+				.handle(controlBusGateway, "startEndpoint").get();
+	}
+
+	@MessagingGateway(defaultRequestChannel = "controlBus.input")
+	public static interface ControlBusGateway {
+
+		@Gateway(payloadExpression = "'@' + args[0] + '.start()'")
+		void startEndpoint(String id);
+
+	}
+
+}
