851c36489482b3802f5b3e84f626643c22ce43f6
Olga Maciaszek-Sharma <olga.maciaszek@gmail.com>
2022-09-06 16:19:25 +0200

    Add Zookeeper discovery client sample.

---
+++ cloud-discovery-zookeeper/src/aotSmokeTest/java/com/example/cloud/discovery/zookeeper/ZookeeperDiscoveryClientApplicationAotTests.java
@@ -0,0 +1,23 @@
+package com.example.cloud.discovery.zookeeper;
+
+import java.time.Duration;
+
+import org.awaitility.Awaitility;
+import org.junit.jupiter.api.Test;
+
+import org.springframework.aot.smoketest.support.assertj.AssertableOutput;
+import org.springframework.aot.smoketest.support.junit.AotSmokeTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@AotSmokeTest
+class ZookeeperDiscoveryClientApplicationAotTests {
+
+	@Test
+	void shouldRegisterWithZookeeper(AssertableOutput output) {
+		Awaitility.await().atMost(Duration.ofSeconds(30))
+				.untilAsserted(() -> assertThat(output).hasLineContaining("Session establishment complete on server"));
+
+	}
+
+}
