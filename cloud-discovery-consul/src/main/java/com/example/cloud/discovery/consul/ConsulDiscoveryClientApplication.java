d9ac510098a08aa20b461d5c6a393222a760018c
Olga Maciaszek-Sharma <olga.maciaszek@gmail.com>
2022-09-06 17:31:17 +0200

    Add Consul Discovery Client sample.

---
+++ cloud-discovery-consul/src/main/java/com/example/cloud/discovery/consul/ConsulDiscoveryClientApplication.java
@@ -0,0 +1,28 @@
+package com.example.cloud.discovery.consul;
+
+import org.springframework.aot.smoketest.thirdpartyhints.NettyRuntimeHints;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportRuntimeHints;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@ImportRuntimeHints(NettyRuntimeHints.class)
+@SpringBootApplication
+public class ConsulDiscoveryClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsulDiscoveryClientApplication.class, args);
+	}
+
+	@RestController
+	static class TestController {
+
+		@GetMapping("/")
+		public String test() {
+			return "test";
+		}
+
+	}
+
+}
