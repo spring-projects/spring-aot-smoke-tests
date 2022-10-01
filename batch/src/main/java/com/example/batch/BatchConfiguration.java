dec417a5d393c7683bd21794f45bf95cf5813554
Mahmoud Ben Hassine <mbenhassine@vmware.com>
2022-10-01 12:22:12 +0200

    Adapt Batch sample to latest changes in Spring Batch and Boot
    See https://github.com/spring-projects/spring-batch/issues/4192
    See https://github.com/spring-projects/spring-boot/wiki/Spring-Boot-3.0.0-M5-Release-Notes#enablebatchprocessing-no-longer-required

--- batch/src/main/java/com/example/batch/BatchConfiguration.java
+++ batch/src/main/java/com/example/batch/BatchConfiguration.java
@@ -2,7 +2,6 @@ package com.example.batch;
 
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
@@ -12,20 +11,19 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration(proxyBeanMethods = false)
-@EnableBatchProcessing
 class BatchConfiguration {
 
 	@Bean
 	public Job job(JobRepository jobRepository, Step step1) {
-		return new JobBuilder("job").repository(jobRepository).start(step1).build();
+		return new JobBuilder("job", jobRepository).start(step1).build();
 	}
 
 	@Bean
 	public Step step1(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
-		return new StepBuilder("step1").tasklet((stepContribution, chunkContext) -> {
+		return new StepBuilder("step1", jobRepository).tasklet((stepContribution, chunkContext) -> {
 			System.out.println("Step 1 running");
 			return RepeatStatus.FINISHED;
-		}).repository(jobRepository).transactionManager(transactionManager).build();
+		}, transactionManager).build();
 	}
 
 }
