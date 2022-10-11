3068182c00e97c8b898aecc55d741c53c894c938
Andy Wilkinson <wilkinsona@vmware.com>
2022-10-11 15:38:21 +0100

    Remove reference to deleted third-party Thymeleaf hints
    They were removed in 71e0d9351a9f06160c111458bd868671f12d55e2

--- security-thymeleaf/src/main/java/com/example/security/thymeleaf/SecurityThymeleafApplication.java
+++ security-thymeleaf/src/main/java/com/example/security/thymeleaf/SecurityThymeleafApplication.java
@@ -1,12 +1,10 @@
 package com.example.security.thymeleaf;
 
-import org.springframework.aot.smoketest.thirdpartyhints.ThymeleafRuntimeHints;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportRuntimeHints;
 
 @SpringBootApplication
-@ImportRuntimeHints(ThymeleafRuntimeHints.class)
 public class SecurityThymeleafApplication {
 
 	public static void main(String[] args) throws Throwable {
