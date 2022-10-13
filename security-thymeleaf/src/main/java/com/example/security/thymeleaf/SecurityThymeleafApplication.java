10f7fc3982bccd0cbff00bbf73804287ac3b2b82
Sébastien Deleuze <sdeleuze@vmware.com>
2022-10-13 14:38:39 +0200

    Fix security-thymeleaf sample
    To be green requires reachability repository 0.2.3 to get
    oracle/graalvm-reachability-metadata#80.

--- security-thymeleaf/src/main/java/com/example/security/thymeleaf/SecurityThymeleafApplication.java
+++ security-thymeleaf/src/main/java/com/example/security/thymeleaf/SecurityThymeleafApplication.java
@@ -1,15 +1,31 @@
 package com.example.security.thymeleaf;
 
+import org.springframework.aot.hint.MemberCategory;
+import org.springframework.aot.hint.RuntimeHints;
+import org.springframework.aot.hint.RuntimeHintsRegistrar;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportRuntimeHints;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.User;
 
 @SpringBootApplication
+@ImportRuntimeHints(SecurityThymeleafApplication.Hints.class)
 public class SecurityThymeleafApplication {
 
 	public static void main(String[] args) throws Throwable {
 		SpringApplication.run(SecurityThymeleafApplication.class, args);
 		Thread.currentThread().join(); // To be able to measure memory consumption
+	}
+
+	static class Hints implements RuntimeHintsRegistrar {
+
+		@Override
+		public void registerHints(RuntimeHints hints, ClassLoader classLoader) {
+			hints.reflection().registerType(UsernamePasswordAuthenticationToken.class,
+					MemberCategory.INVOKE_PUBLIC_METHODS);
+			hints.reflection().registerType(User.class, MemberCategory.INVOKE_PUBLIC_METHODS);
+		}
 	}
 
 }
