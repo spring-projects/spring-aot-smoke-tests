a306687c2f3e1e3813f4c0736c5591dd6498acda
Moritz Halbritter <mkammerer@vmware.com>
2022-10-17 10:45:56 +0200

    Remove obsolete UndertowRuntimeHints

--- servlet-undertow/src/main/java/com/example/servlet/undertow/ServletUndertowApplication.java
+++ servlet-undertow/src/main/java/com/example/servlet/undertow/ServletUndertowApplication.java
@@ -1,12 +1,9 @@
 package com.example.servlet.undertow;
 
-import org.springframework.aot.smoketest.thirdpartyhints.UndertowRuntimeHints;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ImportRuntimeHints;
 
 @SpringBootApplication
-@ImportRuntimeHints(UndertowRuntimeHints.class)
 public class ServletUndertowApplication {
 
 	public static void main(String[] args) throws InterruptedException {
