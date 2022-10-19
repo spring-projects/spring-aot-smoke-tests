2747f02f70c6a68c9f948c02cb5d2b438b3f85a8
Sébastien Deleuze <sdeleuze@vmware.com>
2022-10-19 21:02:39 +0200

    Upgrade to GraalVM 22.3
    Update of .sdkmanrc will be done later since it is not yet
    available on SDKman.
    
    See gh-128

--- ci/images/get-graal-url.sh
+++ ci/images/get-graal-url.sh
@@ -8,6 +8,9 @@ case "$1" in
   graal22_2)
     echo "https://download.bell-sw.com/vm/22.2.0/bellsoft-liberica-vm-openjdk17.0.4+8-22.2.0+2-linux-amd64.tar.gz"
   ;;
+  graal22_3)
+    echo "https://download.bell-sw.com/vm/22.3.0/bellsoft-liberica-vm-core-openjdk17.0.5+8-22.3.0+1-ea-linux-amd64.tar.gz"
+  ;;
   *)
 		echo $"Unknown Graal version"
 		exit 1
