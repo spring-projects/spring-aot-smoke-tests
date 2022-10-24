adf5b41cea1825041754b7533e3d0d4f91f364f4
Christoph Strobl <cstrobl@vmware.com>
2022-10-24 09:57:07 +0200

    Enable SQL logging and verify entity graph is applied

--- data-jpa/src/aotSmokeTest/java/com/example/data/jpa/DataJpaApplicationAotTests.java
+++ data-jpa/src/aotSmokeTest/java/com/example/data/jpa/DataJpaApplicationAotTests.java
@@ -73,8 +73,10 @@ class DataJpaApplicationAotTests {
 	@Test
 	void entityGraph(AssertableOutput output) {
 		Awaitility.await().atMost(Duration.ofSeconds(10)).untilAsserted(() -> {
-			assertThat(output).hasSingleLineContaining(
-					"namedEntityGraph: Book{title='Spring in Action', authors=[Author{name='Craig Walls'}]}");
+			assertThat(output)
+					.hasLineContaining("left join (book_authors a1_0 join author a1_1 on a1_1.id=a1_0.authors_id)")
+					.hasSingleLineContaining(
+							"namedEntityGraph: Book{title='Spring in Action', authors=[Author{name='Craig Walls'}]}");
 		});
 	}
 
