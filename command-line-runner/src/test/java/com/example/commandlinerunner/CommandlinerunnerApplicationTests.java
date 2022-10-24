953dcfe6fb896a5f892cabebb621ab6bcdfad54e
Moritz Halbritter <mkammerer@vmware.com>
2022-10-24 15:46:43 +0200

    Add test case for ApplicationArguments

--- command-line-runner/src/test/java/com/example/commandlinerunner/CommandlinerunnerApplicationTests.java
+++ command-line-runner/src/test/java/com/example/commandlinerunner/CommandlinerunnerApplicationTests.java
@@ -1,8 +1,11 @@
 package com.example.commandlinerunner;
 
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.UseMainMethod;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
@@ -11,7 +14,7 @@ import org.springframework.context.ApplicationContextAware;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest(webEnvironment = WebEnvironment.NONE, useMainMethod = UseMainMethod.ALWAYS)
+@SpringBootTest(webEnvironment = WebEnvironment.NONE, useMainMethod = UseMainMethod.ALWAYS, args = "--test.property=1")
 class CommandlinerunnerApplicationTests implements ApplicationContextAware {
 
 	private ApplicationContext applicationContext;
@@ -38,6 +41,12 @@ class CommandlinerunnerApplicationTests implements ApplicationContextAware {
 	void mainShouldRun() {
 		String property = System.getProperty("main.ran", "false");
 		assertThat(property).isEqualTo("true");
+	}
+
+	@Test
+	void shouldPassArgs(@Autowired ApplicationArguments applicationArguments) {
+		List<String> values = applicationArguments.getOptionValues("test.property");
+		assertThat(values).containsExactly("1");
 	}
 
 }
