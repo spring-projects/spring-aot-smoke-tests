0af95ab570534210f54a21f819b544d3af233b4a
Moritz Halbritter <mkammerer@vmware.com>
2022-10-25 10:28:28 +0200

    Add test case for OutputCaptureExtension

---
+++ command-line-runner/src/test/java/com/example/commandlinerunner/OutputCaptureTests.java
@@ -0,0 +1,20 @@
+package com.example.commandlinerunner;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import org.springframework.boot.test.system.CapturedOutput;
+import org.springframework.boot.test.system.OutputCaptureExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(OutputCaptureExtension.class)
+class OutputCaptureTests {
+
+	@Test
+	void testName(CapturedOutput output) {
+		System.out.println("Hello World!");
+		assertThat(output).contains("Hello World!");
+	}
+
+}
