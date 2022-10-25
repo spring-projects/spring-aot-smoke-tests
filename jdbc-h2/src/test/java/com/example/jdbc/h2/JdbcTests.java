ee5191b37b28885bd96cf6cfd59f663d5199b96c
Moritz Halbritter <mkammerer@vmware.com>
2022-10-25 09:30:46 +0200

    Add test case for transaction rollback

--- jdbc-h2/src/test/java/com/example/jdbc/h2/JdbcTests.java
+++ jdbc-h2/src/test/java/com/example/jdbc/h2/JdbcTests.java
@@ -1,22 +1,34 @@
 package com.example.jdbc.h2;
 
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
 import org.springframework.jdbc.core.JdbcTemplate;
 
-import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @JdbcTest
+@TestMethodOrder(OrderAnnotation.class)
 class JdbcTests {
 
 	@Autowired
 	private JdbcTemplate jdbcTemplate;
 
 	@Test
-	void shouldHaveSchema() {
-		assertThatCode(() -> this.jdbcTemplate.execute("SELECT * FROM authors")).doesNotThrowAnyException();
+	@Order(0)
+	void canInsert() {
+		this.jdbcTemplate.execute("INSERT INTO authors (name) VALUES ('author-1')");
+	}
+
+	@Test
+	@Order(1)
+	void shouldBeIsolated() {
+		Integer count = this.jdbcTemplate.queryForObject("SELECT COUNT(1) FROM authors", int.class);
+		assertThat(count).isZero();
 	}
 
 }
