0ed5498b45419bcdd366a68c76be344a8e322e48
Olga Maciaszek-Sharma <olga.maciaszek@gmail.com>
2022-11-03 17:26:50 +0100

    Add Cloud Gateway smoke tests
    See gh-149

---
+++ cloud/cloud-gateway/src/appTest/java/com/example/cloud/gateway/CloudGatewayApplicationAotTests.java
@@ -0,0 +1,21 @@
+package com.example.cloud.gateway;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.aot.smoketest.support.junit.ApplicationTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ApplicationTest
+public class CloudGatewayApplicationAotTests {
+
+	@Test
+	void shouldRouteRequests(WebTestClient client) {
+		client.get().uri("test-service").exchange().expectStatus().isOk().expectBody()
+				.consumeWith((result) -> assertThat(new String(result.getResponseBodyContent())).isEqualTo("test"));
+		client.get().uri("demo-service").exchange().expectStatus().isOk().expectBody()
+				.consumeWith((result) -> assertThat(new String(result.getResponseBodyContent())).isEqualTo("demo"));
+	}
+
+}
---
+++ cloud/cloud-gateway/src/main/java/org/example/cloud/gateway/RoutesConfig.java
@@ -0,0 +1,20 @@
+package org.example.cloud.gateway;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+
+@Configuration
+class RoutesConfig {
+
+	@Bean
+	RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+		return builder
+				.routes().route("demo_service_route", route -> route.path("/demo-service/**").and()
+						.method(HttpMethod.GET).filters(filter -> filter.stripPrefix(1)).uri("lb://demo-service"))
+				.build();
+	}
+
+}
