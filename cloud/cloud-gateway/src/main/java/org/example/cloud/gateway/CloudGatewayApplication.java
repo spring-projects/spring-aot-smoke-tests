2555967fe4e4d7f87f1a3d44c4cd603b4162e69e
Olga Maciaszek-Sharma <olga.maciaszek@gmail.com>
2022-11-03 17:26:50 +0100

    Add Cloud Gateway smoke tests.

---
+++ cloud/cloud-gateway/src/main/java/org/example/cloud/gateway/CloudGatewayApplication.java
@@ -0,0 +1,13 @@
+package org.example.cloud.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudGatewayApplication.class, args);
+	}
+
+}
