5025acf98fdde09f826e92e73823002fd309a737
Andy Wilkinson <wilkinsona@vmware.com>
2022-11-09 20:51:18 +0000

    Add reflection hints for DTOs in json smoke test

--- boot/json/src/main/java/com/example/json/JsonApplication.java
+++ boot/json/src/main/java/com/example/json/JsonApplication.java
@@ -1,9 +1,15 @@
 package com.example.json;
 
+import com.example.json.model.Dto;
+import com.example.json.model.Dto2;
+import com.example.json.model.Dto3;
+
+import org.springframework.aot.hint.annotation.RegisterReflectionForBinding;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@RegisterReflectionForBinding({Dto.class, Dto2.class, Dto3.class})
 public class JsonApplication {
 
 	public static void main(String[] args) throws InterruptedException {
