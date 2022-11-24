8d6803ecce5918836be7316499656297a8fddb3b
Moritz Halbritter <mkammerer@vmware.com>
2022-11-24 11:58:02 +0100

    Polish RandomPortTests

--- framework/webflux-netty/src/test/java/com/example/webflux/test/RandomPortTests.java
+++ framework/webflux-netty/src/test/java/com/example/webflux/test/RandomPortTests.java
@@ -1,13 +1,14 @@
 package com.example.webflux.test;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
-
 import org.junit.jupiter.api.Test;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.web.reactive.server.WebTestClient;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
 @SpringBootTest(webEnvironment = RANDOM_PORT)
 public class RandomPortTests {
@@ -16,10 +17,13 @@ public class RandomPortTests {
 	int port;
 
 	@Test
-	void check(@Autowired WebTestClient client) {
+	void check() {
 		assertThat(this.port).isNotZero();
+	}
 
-		client.get().uri("/").exchange().expectStatus().isOk().expectBody(String.class).isEqualTo("hi!");
+	@Test
+	void webClientWorks(@Autowired WebTestClient webClient) {
+		webClient.get().uri("/").exchange().expectStatus().isOk().expectBody(String.class).isEqualTo("hi!");
 	}
 
 }
