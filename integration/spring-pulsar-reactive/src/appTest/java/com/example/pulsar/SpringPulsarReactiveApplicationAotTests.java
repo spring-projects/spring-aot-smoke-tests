8255fc0da37da9784962f35d2a52e81e251de236
Soby Chacko <chackos@vmware.com>
2022-12-05 15:13:31 -0500

    Smoke test for Spring Pulsar reactive native app

---
+++ integration/spring-pulsar-reactive/src/appTest/java/com/example/pulsar/SpringPulsarReactiveApplicationAotTests.java
@@ -0,0 +1,21 @@
+package com.example.pulsar;
+
+import org.awaitility.Awaitility;
+import org.junit.jupiter.api.Test;
+import org.springframework.aot.smoketest.support.assertj.AssertableOutput;
+import org.springframework.aot.smoketest.support.junit.ApplicationTest;
+
+import java.time.Duration;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ApplicationTest
+public class SpringPulsarReactiveApplicationAotTests {
+
+	@Test
+	void reactivePulsarListenerMethodReceivesMessage(AssertableOutput output) {
+		Awaitility.await().atMost(Duration.ofSeconds(30))
+				.untilAsserted(() -> assertThat(output).hasSingleLineContaining("Message Received: sample-message-50"));
+	}
+
+}
