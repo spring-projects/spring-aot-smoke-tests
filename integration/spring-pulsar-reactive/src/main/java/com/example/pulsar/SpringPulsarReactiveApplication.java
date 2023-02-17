f7dde045943b57d9ca1727d633ce66252e4feaf5
Chris Bono <cbono@vmware.com>
2023-02-16 18:03:46 -0600

    Update Spring Pulsar versions
    * Spring Pulsar 0.1.1-SNAPSHOT
    * Apache Pulsar 2.11.0

--- integration/spring-pulsar-reactive/src/main/java/com/example/pulsar/SpringPulsarReactiveApplication.java
+++ integration/spring-pulsar-reactive/src/main/java/com/example/pulsar/SpringPulsarReactiveApplication.java
@@ -1,5 +1,7 @@
 package com.example.pulsar;
 
+import org.apache.pulsar.reactive.client.api.MessageSpec;
+
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -20,10 +22,8 @@ public class SpringPulsarReactiveApplication {
 	@Bean
 	ApplicationRunner sendMessageToTopicOnAppStartup(ReactivePulsarTemplate<String> reactivePulsarTemplate) {
 		String topic = "graalvm-demo-topic-reactive";
-		return args -> {
-			Flux.range(0, 100).map((i) -> "sample-message-" + i)
-					.as(messages -> reactivePulsarTemplate.send(topic, messages)).subscribe();
-		};
+		return args -> Flux.range(0, 100).map((i) -> MessageSpec.of("sample-message-" + i))
+				.as((msgs) -> reactivePulsarTemplate.send(topic, msgs)).subscribe();
 	}
 
 	@ReactivePulsarListener(subscriptionName = "graalvm-demo-subscription-reactive",
