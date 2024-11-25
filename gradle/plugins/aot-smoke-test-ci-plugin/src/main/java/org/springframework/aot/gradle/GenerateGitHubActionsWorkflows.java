cfa883d8c9fcf7fb69bec1d6afcc57dd0954ac87
Andy Wilkinson <andy.wilkinson@broadcom.com>
2024-11-25 10:13:52 +0000

    Start scheduling the 3.2.x tests again

--- gradle/plugins/aot-smoke-test-ci-plugin/src/main/java/org/springframework/aot/gradle/GenerateGitHubActionsWorkflows.java
+++ gradle/plugins/aot-smoke-test-ci-plugin/src/main/java/org/springframework/aot/gradle/GenerateGitHubActionsWorkflows.java
@@ -78,10 +78,8 @@ public abstract class GenerateGitHubActionsWorkflows extends DefaultTask {
 		try (PrintWriter writer = new PrintWriter(new FileWriter(workflowFile))) {
 			writer.println("name: " + workflowName);
 			writer.println("on:");
-			if (!"3.2.x".equals(getGitBranch().get())) {
-				writer.println("  schedule:");
-				writer.println("    - cron : '" + getCronSchedule().get() + "'");
-			}
+			writer.println("  schedule:");
+			writer.println("    - cron : '" + getCronSchedule().get() + "'");
 			writer.println("  workflow_dispatch:");
 			writer.println("jobs:");
 			smokeTest.tests().forEach((test) -> {
