f752d300ea4a3bdcc7da20fa1916455294035381
Olga Maciaszek-Sharma <olga.maciaszek-sharma@broadcom.com>
2025-05-19 19:26:42 +0200

    Adjust to changes in Cloud. Refactor.

--- cloud/cloud-gateway/src/main/java/org/example/cloud/gateway/config/RoutesConfig.java
+++ cloud/cloud-gateway/src/main/java/org/example/cloud/gateway/config/RoutesConfig.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package org.example.cloud.gateway;
+package org.example.cloud.gateway.config;
 
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
