3a146c62bfe0d649b54b8e30ebe697ddbcd4fad6
Olga Maciaszek-Sharma <olga.maciaszek-sharma@broadcom.com>
2025-05-30 15:38:23 +0200

    Add smoke tests for Spring Cloud Gateway MVC.
    Signed-off-by: Olga Maciaszek-Sharma <olga.maciaszek-sharma@broadcom.com>

---
+++ cloud/cloud-gateway-mvc/src/appTest/java/com/example/cloud/gateway/CloudGatewayApplicationAotTests.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2022-2025 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.cloud.gateway;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.aot.smoketest.support.junit.ApplicationTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ApplicationTest
+public class CloudGatewayApplicationAotTests {
+
+	@Test
+	void shouldRouteRequests(WebTestClient client) {
+		client.get()
+			.uri("test-service")
+			.exchange()
+			.expectStatus()
+			.isOk()
+			.expectBody()
+			.consumeWith((result) -> assertThat(new String(result.getResponseBodyContent())).isEqualTo("test"));
+		client.get()
+			.uri("demo-service")
+			.exchange()
+			.expectStatus()
+			.isOk()
+			.expectBody()
+			.consumeWith((result) -> assertThat(new String(result.getResponseBodyContent())).isEqualTo("demo"));
+	}
+
+}
