7a6789f8054cbc6ad902d56064566c14686a893f
Stéphane Nicoll <stephane.nicoll@broadcom.com>
2025-07-15 10:54:37 +0200

    Adapt Spring GraphQL smoke tests to Spring Boot 4.0
    See gh-275

--- graphql/graphql-webflux/src/test/java/com/example/graphql/webflux/RandomPortTests.java
+++ graphql/graphql-webflux/src/test/java/com/example/graphql/webflux/RandomPortTests.java
@@ -23,7 +23,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.graphql.tester.AutoConfigureHttpGraphQlTester;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.web.server.test.LocalServerPort;
 import org.springframework.graphql.test.tester.HttpGraphQlTester;
 import org.springframework.graphql.test.tester.WebSocketGraphQlTester;
 import org.springframework.web.reactive.socket.client.ReactorNettyWebSocketClient;
