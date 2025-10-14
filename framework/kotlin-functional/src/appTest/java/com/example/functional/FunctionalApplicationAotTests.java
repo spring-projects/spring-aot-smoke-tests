34486ec905108accca4dc8954e0901b3236df02e
Sébastien Deleuze <sdeleuze@users.noreply.github.com>
2025-10-14 11:44:18 +0200

    Fix tests format in kotlin-functional

--- framework/kotlin-functional/src/appTest/java/com/example/functional/FunctionalApplicationAotTests.java
+++ framework/kotlin-functional/src/appTest/java/com/example/functional/FunctionalApplicationAotTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2022-2024 the original author or authors.
+ * Copyright 2022-2025 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -40,11 +40,11 @@ public class FunctionalApplicationAotTests {
 	@Test
 	void endpointsAreConfigured(WebTestClient client) {
 		client.get()
-				.exchange()
-				.expectStatus()
-				.isOk()
-				.expectBody()
-				.consumeWith((result) -> assertThat(new String(result.getResponseBodyContent())).isEqualTo("hi!"));
+			.exchange()
+			.expectStatus()
+			.isOk()
+			.expectBody()
+			.consumeWith((result) -> assertThat(new String(result.getResponseBodyContent())).isEqualTo("hi!"));
 	}
 
 }
