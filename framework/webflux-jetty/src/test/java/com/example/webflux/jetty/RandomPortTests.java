46c9193ca1ebf8c0b3195c2d8c2cb35d7e0da25b
Stéphane Nicoll <stephane.nicoll@broadcom.com>
2025-10-14 17:36:08 +0200

    Adapt tests to recent changes in Spring Boot

--- framework/webflux-jetty/src/test/java/com/example/webflux/jetty/RandomPortTests.java
+++ framework/webflux-jetty/src/test/java/com/example/webflux/jetty/RandomPortTests.java
@@ -21,12 +21,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.webtestclient.AutoConfigureWebTestClient;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
 @SpringBootTest(webEnvironment = RANDOM_PORT)
+@AutoConfigureWebTestClient
 public class RandomPortTests {
 
 	@LocalServerPort
--- framework/webmvc-jetty/src/test/java/com/example/webmvc/jetty/RandomPortTests.java
+++ framework/webmvc-jetty/src/test/java/com/example/webmvc/jetty/RandomPortTests.java
@@ -20,6 +20,7 @@ import org.junit.jupiter.api.Test;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.resttestclient.TestRestTemplate;
+import org.springframework.boot.resttestclient.autoconfigure.AutoConfigureTestRestTemplate;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.ResponseEntity;
@@ -28,6 +29,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
 @SpringBootTest(webEnvironment = RANDOM_PORT)
+@AutoConfigureTestRestTemplate
 class RandomPortTests {
 
 	@LocalServerPort
