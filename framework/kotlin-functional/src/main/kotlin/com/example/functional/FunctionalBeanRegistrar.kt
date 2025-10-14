ce2bd79a88af9e05eb077659199485b3f3974787
Sébastien Deleuze <sdeleuze@users.noreply.github.com>
2025-10-14 11:44:00 +0200

    Fix deprecation warning in kotlin-functional
    This commit refines the kotlin-functional sample
    to use the new BeanRegistrarDsl instead of the deprecated
    BeanDefinitionDsl.

--- framework/kotlin-functional/src/main/kotlin/com/example/functional/FunctionalBeanRegistrar.kt
+++ framework/kotlin-functional/src/main/kotlin/com/example/functional/FunctionalBeanRegistrar.kt
@@ -16,15 +16,22 @@
 
 package com.example.functional
 
-import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.runApplication
-import org.springframework.context.annotation.Import
+import org.springframework.beans.factory.BeanRegistrarDsl
+import org.springframework.boot.CommandLineRunner
+import org.springframework.web.reactive.function.server.ServerResponse
+import org.springframework.web.reactive.function.server.router
 
-@SpringBootApplication
-@Import(FunctionalBeanRegistrar::class)
-class FunctionalApplication
+class FunctionalBeanRegistrar: BeanRegistrarDsl({
+    registerBean<Foo>()
+    registerBean(::endpoints)
+})
 
-fun main(args: Array<String>) {
-	runApplication<FunctionalApplication>(*args)
+fun endpoints() = router {
+    GET("/") { ServerResponse.ok().bodyValue("hi!") }
 }
 
+class Foo : CommandLineRunner {
+    override fun run(vararg args: String) {
+        println("Hello world!")
+    }
+}
