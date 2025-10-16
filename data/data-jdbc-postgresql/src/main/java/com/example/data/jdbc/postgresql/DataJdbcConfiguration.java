ecdd76875360529b848ee05d3c7e7737e977c930
Christoph Strobl <christoph.strobl@broadcom.com>
2025-10-16 08:53:43 +0200

    Adapt data-jdbc test setup for AOT repository processing
    See gh-287
    
    Provide the JdbcDialect to avoid db access attempt during AOT phase.

--- data/data-jdbc-postgresql/src/main/java/com/example/data/jdbc/postgresql/DataJdbcConfiguration.java
+++ data/data-jdbc-postgresql/src/main/java/com/example/data/jdbc/postgresql/DataJdbcConfiguration.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2022-2024 the original author or authors.
+ * Copyright 2022-present the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,11 +16,18 @@
 
 package com.example.data.jdbc.postgresql;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jdbc.core.dialect.JdbcDialect;
+import org.springframework.data.jdbc.core.dialect.JdbcPostgresDialect;
 import org.springframework.data.jdbc.repository.config.EnableJdbcRepositories;
 
 @Configuration
 @EnableJdbcRepositories
 class DataJdbcConfiguration {
 
+	@Bean
+	JdbcDialect dialect() {
+		return JdbcPostgresDialect.INSTANCE;
+	}
 }
