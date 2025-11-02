b7ff23b0389ae01ff3576f4e9a833391e409b253
Stéphane Nicoll <stephane.nicoll@broadcom.com>
2025-11-02 10:41:08 +0100

    Adapt to changes in Spring Boot 4.0.x

--- framework/webflux-jetty/src/test/java/com/example/webflux/jetty/AutoConfigureWebTestClientTests.java
+++ framework/webflux-jetty/src/test/java/com/example/webflux/jetty/AutoConfigureWebTestClientTests.java
@@ -23,8 +23,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.boot.webtestclient.AutoConfigureWebTestClient;
-import org.springframework.boot.webtestclient.WebTestClientBuilderCustomizer;
+import org.springframework.boot.webtestclient.autoconfigure.AutoConfigureWebTestClient;
+import org.springframework.boot.webtestclient.autoconfigure.WebTestClientBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
