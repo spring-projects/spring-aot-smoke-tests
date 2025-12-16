78f2e8a383d31bfa997452b0b2f53f9d64cec8a3
Andy Wilkinson <andy.wilkinson@broadcom.com>
2025-12-16 19:55:11 +0000

    Add a smoke test for Log4j2 configured using Spring XML

---
+++ boot/logging-log4j2-spring-xml/src/appTest/java/com/example/log4j2/springxml/Log4j2SpringXmlApplicationAotTests.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2022-2024 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.log4j2.springxml;
+
+import java.time.Duration;
+
+import org.awaitility.Awaitility;
+import org.junit.jupiter.api.Test;
+
+import org.springframework.aot.smoketest.support.assertj.AssertableOutput;
+import org.springframework.aot.smoketest.support.junit.ApplicationTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ApplicationTest
+class Log4j2SpringXmlApplicationAotTests {
+
+	@Test
+	void expectedLoggingIsProduced(AssertableOutput output) {
+		Awaitility.await().atMost(Duration.ofSeconds(30)).untilAsserted(() -> {
+			assertThat(output).hasNoLinesContaining("Trace message")
+				.hasSingleLineContaining("main | DEBUG | com.example.log4j2.springxml.CLR | Debug message")
+				.hasSingleLineContaining("main | INFO  | com.example.log4j2.springxml.CLR | Info message")
+				.hasSingleLineContaining("main | WARN  | com.example.log4j2.springxml.CLR | Warn message")
+				.hasSingleLineContaining("main | ERROR | com.example.log4j2.springxml.CLR | Error message")
+				.hasSingleLineContaining("main | INFO  | com.example.log4j2.springxml.CLR | Info with parameters: 1")
+				.hasSingleLineContaining("main | ERROR | com.example.log4j2.springxml.CLR | Error with stacktrace")
+				.hasSingleLineContaining("java.lang.RuntimeException: Boom")
+				.hasSingleLineContaining("at com.example.log4j2.springxml.CLR.run(CLR.java");
+		});
+	}
+
+}
