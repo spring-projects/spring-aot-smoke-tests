8b82dc50dbd8fc4465cfc4b9549f2874ce0b1cca
Andy Wilkinson <andy.wilkinson@broadcom.com>
2025-12-16 18:57:08 +0000

    Add a smoke test for Log4j2

---
+++ boot/logging-log4j2/src/main/java/com/example/log4j2/CLR.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2022-2024 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.log4j2;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CLR implements CommandLineRunner {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(CLR.class);
+
+	@Override
+	public void run(String... args) throws Exception {
+		LOGGER.trace("Trace message");
+		LOGGER.debug("Debug message");
+		LOGGER.info("Info message");
+		LOGGER.warn("Warn message");
+		LOGGER.error("Error message");
+
+		LOGGER.info("Info with parameters: {}", 1);
+		LOGGER.error("Error with stacktrace", new RuntimeException("Boom"));
+	}
+
+}
